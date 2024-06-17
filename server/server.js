fae4a2638f071c1abca1612ac8c26876c5820443
BoobOO <boobo55555@outlook.com>
2024-06-17 09:56:04 -0700

    just add gitignore file

--- server/server.js
+++ server/server.js
@@ -16,6 +16,24 @@ sequelize.authenticate()
     })
 
 const server = new createServer((req, res) => {
+
+    const User = sequelize.define(
+        'User',
+        {
+          // Model attributes are defined here
+          firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          lastName: {
+            type: DataTypes.STRING,
+            // allowNull defaults to true
+          },
+        },
+        {
+          // Other model options go here
+        },
+      );
     
 })
 
